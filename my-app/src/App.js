f103c90ccd3c9fe2f6eda0a580abb2fe625356e7
Allen Wei <weihr1011@gmail.com>
2022-12-23 21:21:02 -0500

    initializing styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Leftpart } from "./components/Leftpart";
 import { Rightpart } from "./components/Rightpart";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Container className="">
+        <Container className="container d-flex align-items-center justify-content-center vh-100">
           <Row>
             <Leftpart />
             <Rightpart />
--- my-app/src/components/Rightpart.js
+++ my-app/src/components/Rightpart.js
@@ -4,16 +4,68 @@ import { Col } from "react-bootstrap";
 export const Rightpart = () => {
   return (
     <Col md={6} className="col-md-6">
-      <div className=" mb-4">
-        <p className="">
+      <div className="trail text-center mb-4">
+        <p className="mb-0">
           <strong>Try it free 7 days</strong> then $20/mo. thereafter
         </p>
       </div>
-      <Form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+      <Form method="post">
+        <div className="mb-4">
+          <Form.Group className="mb-4">
+            <input
+              className="w-100 form-control"
+              type="text"
+              name="firstname"
+              id="first-Name"
+              placeholder="First Name"
+              aria-label="firstname"
+              required
+            />
+          </Form.Group>
+          <div className="mb-4">
+            <Form.Control
+              className="w-100 form-control"
+              type="text"
+              id="lastname"
+              name="lastname"
+              placeholder="Last Name"
+              aria-label="lastname"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="w-100 form-control"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email Address"
+              aria-label="email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="w-100 form-control"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+        </div>
+        <div className="mb-2">
+          <button className="btn w-100">CLAIM YOUR FREE TRIAL</button>
+        </div>
+        <div className="w-100 text-muted text-center">
+          <small className="">
+            By clicking the button, you are agreeing to our
+            <a className="text-danger" href="{}">
+              Terms and Services
+            </a>
+          </small>
+        </div>
       </Form>
     </Col>
   );
